a8cc8e8c9d05935f926bb518012e8884b2b1848d
Hazael Lopez <elhazael134@gmail.com>
2020-04-22 23:08:31 -0600

    Concatenamos un Strign con un int... Pero primero lo convertimos con el metodo str(RESIVE UN ARGUMENTO TIPO NUMERO INT O FLOAT DOBLE)

--- String_En_Python.py
+++ String_En_Python.py
@@ -9,3 +9,8 @@ espacio = " "
 nombre = "Hazael"
 print(mensaje + espacio + nombre)
 
+numero_uno = 4
+numero_dos = 6
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+print("El resultado de la suma es: " + resultado)
