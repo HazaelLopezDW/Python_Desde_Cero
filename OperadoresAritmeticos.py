fca3043d6445d4b4b92ff5d384692c022dd9a858
Hazael Lopez <elhazael134@gmail.com>
2020-04-24 09:54:37 -0600

    Potenciacion en Python

--- OperadoresAritmeticos.py
+++ OperadoresAritmeticos.py
@@ -15,3 +15,9 @@ numero_uno = 5
 numero_dos = 4
 resultado = numero_uno * numero_dos
 print("El resultado de la multiplicacion es: " + str(resultado))
+print("")
+print("Operador Aritmetico Potenciacion(**)")
+numero_uno = 2
+numero_dos = 5
+resultado = numero_uno ** numero_dos
+print("El resultado de la Potenciacion es: " + str(resultado))
