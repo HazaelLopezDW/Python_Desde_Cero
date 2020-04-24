9a665486c47e6a1a68c53fd53d19819e9bfcc88c
Hazael Lopez <elhazael134@gmail.com>
2020-04-24 10:35:04 -0600

    Vemos todos los tipos de datos en Python

--- TiposDeDatos.py
+++ TiposDeDatos.py
@@ -2,3 +2,18 @@
 print("Tipos de datos enteros")
 numero = 15
 print("El tipo de dato de la variable numero es: " , type(numero))
+print("")
+#Tipos de datos Flotante 
+print("Tipos de datos enteros")
+numero_flotante = 15.5
+print("El tipo de dato de la variable numero_flotante es: " , type(numero_flotante))
+print("")
+#Tipos de datos complejo 
+print("Tipos de datos enteros")
+numero_complejo = 5 + 6j
+print("El tipo de dato de la variable numero_flotante es: " , type(numero_complejo))
+print("")
+#Tipos de datos String 
+print("Tipos de datos String")
+nombre = "Walter Hazael"
+print("El tipo de dato de la variable numero_flotante es: " , type(nombre))
