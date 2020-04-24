f3dd8e8d299fc8c388a969a0748fcc13d3d117f2
Hazael Lopez <elhazael134@gmail.com>
2020-04-24 17:25:20 -0600

    Creamos un programa que pida el nombre y dos numeros a operar en una suma(+)

---
+++ EntradasDeDatosDesDeElTeclado1.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cual es tu nombre?:")
+print("Hola" + nombre + ", vamos a realizar una suma")
+
+num_uno = int(input("Porfavor introduce el primer valor entero: "))
+num_dos = int(input("Porfavor introduce el segundo valor entero: "))
+resultado = num_uno + num_dos
+print(nombre + " El resultado de la su es: " , resultado)
