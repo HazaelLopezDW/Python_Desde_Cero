2475e80b85dffd7f4a788d245b949608f3a6b8ca
Hazael Lopez <elhazael134@gmail.com>
2020-04-24 17:26:37 -0600

    Simplemente reparamos el hola de nuestro nombre

--- EntradasDeDatosDesDeElTeclado1.py
+++ EntradasDeDatosDesDeElTeclado1.py
@@ -1,5 +1,5 @@
 nombre = input("¿Cual es tu nombre?:")
-print("Hola" + nombre + ", vamos a realizar una suma")
+print("Hola " + nombre + ", vamos a realizar una suma")
 
 num_uno = int(input("Porfavor introduce el primer valor entero: "))
 num_dos = int(input("Porfavor introduce el segundo valor entero: "))
