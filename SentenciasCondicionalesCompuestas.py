0ef39d57acb9d1eb693a950ac61f02112119ea28
Hazael Lopez <elhazael134@gmail.com>
2020-04-26 13:06:27 -0600

    Escribimos un programa que muestre si un alumno aprobo o reprobo  vemos el metrodo round(float, #Desimales)

---
+++ SentenciasCondicionalesCompuestas.py
@@ -0,0 +1,24 @@
+"""
+    Veremos las sentencias condicionales compuestas en Python
+    Estas son las sentencias condicionales conocidas como If Else
+    Como ya las vimos en Java
+"""
+#Declaramos nuestras variables a usar
+
+print("Sistema para calcular el promedio de un alumno.")
+nombre = input("Para comezar, ¿Cual es tu nombre?: ")
+
+matematicas = int(input( nombre + " ¿Cual es tu calificación en matematicas: "))
+quimica = int(input(nombre + " ¿Cual es tu calificación en quimica: "))
+biologia = int(input(nombre + " ¿Cual es tu calificación en biologia: "))
+
+promedio = (matematicas + quimica + biologia)/3
+
+if (promedio >= 6):
+    print('Felicidades ' + nombre + ' "Aprobastes El Curso" con un promedio de:' , promedio)
+    print('Felicidades ' + nombre + ' "Aprobastes El Curso" con un promedio de:' , round(promedio,2))
+else:
+    print('Felicidades ' + nombre + ' "Aprobastes El Curso" con un promedio de:' + str(promedio))
+    print('Felicidades ' + nombre + ' "Aprobastes El Curso" con un promedio de:' + str(round(promedio,2)))
+
+print("Fin....")
